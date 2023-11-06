cc722c4b7362ccfc150e9fa52d6f3b678f5736f3
Dhruv Thanawala <126741154+dtDhruv@users.noreply.github.com>
2023-11-06 20:35:26 +0530

    Create flask.sh

---
+++ flask.sh
@@ -0,0 +1,13 @@
+#! usr/bin/bash
+
+read -p 'Enter Name of Flask App: ' name
+mkdir "$name"
+cd "$name"
+mkdir templates
+mkdir static
+touch app.py
+chmod +x app.py
+python3 -m venv venv
+source venv/bin/activate
+pip install Flask
+code .
